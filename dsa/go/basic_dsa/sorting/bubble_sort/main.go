e9e60bf17faab794d51e69697f6aded9c413d70c
Roshan Paturkar <paturkarr@gmail.com>
2024-12-08 22:41:21 +0530

    bubble sort implemented

---
+++ dsa/go/basic_dsa/sorting/bubble_sort/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func bubbleDort(unsoertedList []int) []int {
+	n := len(unsoertedList)
+
+	for i:=n-1; i>=0; i-- {
+		swap := false;
+
+		for j:=0; j<i; j++ {
+			if unsoertedList[j] > unsoertedList[j+1] {
+				unsoertedList[j], unsoertedList[j+1] = unsoertedList[j+1], unsoertedList[j]
+				swap = true
+			}
+		}
+
+		if !swap {
+			return unsoertedList
+		}
+	}
+
+	return unsoertedList
+}
+
+func main() {
+	unsortedList := []int{5,7,2,5,1,4,8,3,10,-1,45,100,34,22,65,87,65,0}
+	
+	sortedList := bubbleDort(unsortedList)
+	fmt.Println("Sorted list by bubble sort: ", sortedList)
+}
