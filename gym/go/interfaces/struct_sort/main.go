b098a3ad2c6785f39aeadbac246b4ea60dbfb1d6
Roshan Paturkar <paturkarr@gmail.com>
2025-02-09 16:30:54 +0530

    sort a slice of custom structs using the sort package

---
+++ gym/go/interfaces/struct_sort/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Interface interface {
+        // Find number of elements in collection
+        Len() int
+        
+        // Less method is used for identifying which elements among index i and j are lesser and is used for sorting
+        Less(i, j int) bool
+        
+        // Swap method is used for swapping elements with indexes i and j
+        Swap(i, j int)
+}
+
+type Human struct {
+    name string
+    age  int
+}
+
+// AgeFactor implements sort.Interface that sorts the slice based on age field.
+type AgeFactor []Human
+func (a AgeFactor) Len() int           { return len(a) }
+func (a AgeFactor) Less(i, j int) bool { return a[i].age < a[j].age }
+func (a AgeFactor) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+func main() {
+    audience := []Human{
+        {"Alice", 35},
+        {"Bob", 45},
+        {"James", 25},
+    }
+    sort.Sort(AgeFactor(audience))
+    fmt.Println(audience) 
+}
