06f37736a8f40d6b67c4c563816cb7194ba3e5d8
Roshan Paturkar <paturkarr@gmail.com>
2025-02-23 13:58:00 +0530

    fixed window - maximum subarray sum

---
+++ dsa/go/3-two_pointers/3-sliding_window/1-subarray_sum_fixed/main.go
@@ -0,0 +1,58 @@
+// Given an array (list) nums consisted of only non-negative integers, find the largest sum among all subarrays of length k in nums.
+// For example, if the input is nums = [1, 2, 3, 7, 4, 1], k = 3,
+//then the output would be 14 as the largest length 3 subarray sum is given by [3, 7, 4] which sums to 14.
+
+package main
+
+import (
+	"fmt"
+)
+
+// func maxSumSubarray(nums []int, k int) int {
+// 	sum := 0
+// 	maxSum := 0
+
+// 	for i := range nums {
+// 		sum += nums[i]
+
+// 		if i >= k-1 {
+// 			maxSum = max(maxSum, sum)
+// 			sum -= nums[i-k+1]
+// 		}
+// 	}
+
+// 	return maxSum
+// }
+
+func maxSumSubarray(nums []int, k int) int {
+	sum := 0
+	maxSum := 0
+	
+	for i := range k {
+		sum += nums[i]
+	}
+	maxSum = max(maxSum, sum)
+	for i := k; i < len(nums); i++ {
+		sum += nums[i] - nums[i-k]
+		maxSum = max(maxSum, sum)
+	}
+	
+	return maxSum
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	nums := []int{1, 2, 3, 7, 4, 1}
+	k := 3
+	fmt.Println(maxSumSubarray(nums, k))
+	
+	nums = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	k = 4
+	fmt.Println(maxSumSubarray(nums, k))
+}
