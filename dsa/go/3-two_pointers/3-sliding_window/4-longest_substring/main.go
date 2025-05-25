824c17608e78422f05f97c46e9133965633bc097
Roshan Paturkar <paturkarr@gmail.com>
2025-05-25 14:29:57 +0530

    code fixed

--- dsa/go/3-two_pointers/3-sliding_window/4-longest_substring/main.go
+++ dsa/go/3-two_pointers/3-sliding_window/4-longest_substring/main.go
@@ -1,11 +1,3 @@
-// Find the length of the longest substring of a given string without repeating characters.
-// Input: abccabcabcc
-// Output: 3
-// Explanation: The longest substrings are abc and cab, both of length 3.
-// Input: aaaabaaa
-// Output: 2
-// Explanation: ab is the longest substring, with a length of 2.
-
 package main
 
 import (
