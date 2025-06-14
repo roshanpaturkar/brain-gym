d82051bd6fce88755905542849df49c0aaf25295
Roshan Paturkar <paturkarr@gmail.com>
2025-06-15 04:05:23 +0530

    Add initial implementation of recursion with factorial function

---
+++ dsa/go/4-depth_first_search/1-recursion/README.md
@@ -0,0 +1,3 @@
+# Recursion
+
+Recursion is one of the most important concepts in computer science. Simply speaking, recursion is the process of a function calling itself.
---
+++ dsa/go/4-depth_first_search/1-recursion/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func main() {
+	n := 5
+	result := factorial(n)
+	fmt.Printf("Factorial of %d is %d\n", n, result)
+}
