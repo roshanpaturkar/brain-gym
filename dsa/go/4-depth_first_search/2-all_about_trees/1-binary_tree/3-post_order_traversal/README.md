c4bbed61242a817a12a7e2d3755a68b0468620c6
Roshan Paturkar <paturkarr@gmail.com>
2025-06-16 06:05:50 +0530

    Add implementation of post-order traversal for binary trees

---
+++ dsa/go/4-depth_first_search/2-all_about_trees/1-binary_tree/3-post_order_traversal/README.md
@@ -0,0 +1,5 @@
+# Post-order Traversal
+
+Post-order traversal visits the left subtree first, then the right subtree, and finally the current node. The diagram below shows the traversal order of a post-order traversal on a binary tree.
+
+# ![Post-order Traversal](https://assets.algo.monster/postorder_traversal.gif)
---
+++ dsa/go/4-depth_first_search/2-all_about_trees/1-binary_tree/3-post_order_traversal/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Node struct {
+    val   int
+    left  *Node
+    right *Node
+}
+
+func postOrderTraversal(root *Node) {
+    if root != nil {
+        postOrderTraversal(root.left)
+        postOrderTraversal(root.right)
+        fmt.Println(root.val)
+    }
+}
+
+func buildTree(nodes []string, pos *int) *Node {
+    val := nodes[*pos]
+    *pos++
+    if val == "x" {
+        return nil
+    }
+    v, _ := strconv.Atoi(val)
+    left := buildTree(nodes, pos)
+    right := buildTree(nodes, pos)
+    return &Node{v, left, right}
+}
+
+func splitWords(s string) []string {
+    if s == "" {
+        return []string{}
+    }
+    return strings.Split(s, " ")
+}
+
+func main() {
+	input := "5 4 3 x x 8 x x 6 x x"
+
+	pos := 0
+	root := buildTree(splitWords(input), &pos)
+
+	fmt.Println("Post-order traversal of the binary tree:")
+	postOrderTraversal(root)
+
+	input = "1 2 4 x x 5 x x 3 6 x x 7 x x"
+
+	pos = 0
+	root = buildTree(splitWords(input), &pos)
+	fmt.Println("Post-order traversal of the binary tree:")
+	postOrderTraversal(root)
+}
