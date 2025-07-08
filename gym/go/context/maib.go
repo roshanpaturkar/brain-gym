f99912f4b1d8335316a93c1416a4cbad30935bf0
Roshan Paturkar <paturkarr@gmail.com>
2025-07-08 16:25:57 +0530

    context handling and ensure proper HTTP server setup

--- gym/go/context/maib.go
+++ gym/go/context/maib.go
@@ -3,14 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"time"
 )
 
 func main() {
-	ctx := context.Background()
+	// ctx := context.Background()
 
-	contextTimeout(ctx)
-	contextWithValue(ctx)
+	// contextTimeout(ctx)
+	// contextWithValue(ctx)
+
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func contextTimeout(ctx context.Context) {
@@ -46,5 +50,19 @@ func contextWithValue(ctx context.Context) {
 		fmt.Println("User authenticated with userId:", userId)
 	} else {
 		fmt.Println("Protected rout, userId is missing!")
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), 2*time.Second)
+	defer cancel()
+
+	select {
+		case <- time.After(3*time.Second):
+			fmt.Println("API response!")
+		case <- ctx.Done():
+			fmt.Println("Context expired!")
+			http.Error(w, "Request contect timeout", http.StatusRequestTimeout)
+			return
 	}
 }
